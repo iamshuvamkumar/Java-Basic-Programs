a29a109f366f3ecf4ffafcff428ddca16d1744f8
Shivam Kumar <126269823+iamshuvamkumar@users.noreply.github.com>
2023-06-06 21:18:36 +0530

    Update Transpose.java

--- Array/Transpose.java
+++ Array/Transpose.java
@@ -7,13 +7,13 @@ class Transpose{
 		int [][]x  = new int [3][3];
 		
 		for(int i=0;i<x.length;i++){
-			for(int j=0;j<x[i].length;j++){
+			for(int j=0;j<x[i].length;j++){  
 				x[i][j]=sc.nextInt();
 			}
 		}
 		for(int i=0;i<x.length;i++){
 			for(int j=0;j<x[i].length;j++){
-				System.out.print(" "+x[i][j]);
+				System.out.print(" "+x[j][i]);
 			}
 			System.out.println();
 		}
